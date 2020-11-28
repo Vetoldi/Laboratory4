64fbf5a277ec167b7057f45fb552a82c78b52c84
Vetoldi <vetol9186@gmail.com>
2020-11-28 22:10:29 +0200

    Initial commit 	new file:   "Git - \320\257\321\200\320\273\321\213\320\272.lnk" 	new file:   Laba2/.gitattributes 	new file:   Laba2/.gitignore 	new file:   Laba2/ConsoleApplication2/App.config 	new file:   Laba2/ConsoleApplication2/ConsoleApplication2.csproj 	new file:   Laba2/ConsoleApplication2/Program.cs 	new file:   Laba2/ConsoleApplication2/Properties/AssemblyInfo.cs 	new file:   Laba2/ConsoleLol.sln 	new file:   Laba2/ConsoleLol/App.config 	new file:   Laba2/ConsoleLol/ConsoleLol.csproj 	new file:   Laba2/ConsoleLol/Program.cs 	new file:   Laba2/ConsoleLol/Properties/AssemblyInfo.cs 	new file:   Laba2/Task1/Address.cs 	new file:   Laba2/Task1/Program.cs 	new file:   Laba2/Task1/Task1.csproj 	new file:   Laba2/Task2/Program.cs 	new file:   Laba2/Task2/Rectangle.cs 	new file:   Laba2/Task2/Task2.csproj 	new file:   Laba2/Task3/Author.cs 	new file:   Laba2/Task3/Book.cs 	new file:   Laba2/Task3/Content.cs 	new file:   Laba2/Task3/Program.cs 	new file:   Laba2/Task3/Task3.csproj 	new file:   Laba2/Task3/Title.cs 	new file:   Laba2/Task4/Figure.cs 	new file:   Laba2/Task4/Point.cs 	new file:   Laba2/Task4/Program.cs 	new file:   Laba2/Task4/Task4.csproj 	new file:   Laba2/Task5/Program.cs 	new file:   Laba2/Task5/Task5.csproj 	new file:   Laba2/Task5/User.cs 	new file:   Laba2/Task6/Converter.cs 	new file:   Laba2/Task6/Program.cs 	new file:   Laba2/Task6/Task6.csproj 	new file:   Laba2/Task7/Employee.cs 	new file:   Laba2/Task7/Program.cs 	new file:   Laba2/Task7/Task7.csproj 	new file:   Laba2/Task8/Invoice.cs 	new file:   Laba2/Task8/Program.cs 	new file:   Laba2/Task8/Task8.csproj

---
+++ Laba2/Task8/Invoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task8
+{
+    public class Invoice
+    {
+         public readonly int  account ;
+         public readonly string customer;
+         public readonly string provider;
+        public Invoice(int z, string x, string c)
+        {
+            account = z;
+            customer = x;
+            provider = c;
+
+        }
+           private string article ;
+           private int quantitу ;
+
+        public double CalculatorNds(double quantiti,double price, double Nds_perc )
+        {
+            double res = quantiti*price*Nds_perc;
+            return res;
+        }
+        public double CalculatorNds(double price, double quantiti)
+        {
+            double res = quantiti * price;
+            return res;
+        }
+
+    }
+
+}
+    
---
+++ Laba2/Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice  p = new Invoice(1,"kek","lol");
+
+            double totalprice = p.CalculatorNds(12,13);
+            Console.WriteLine("total price :"+ totalprice);
+        }
+    }
+}
